acbd8aa1788c670101822fb93e11a755aee776d5
Ayelen Valdez <ayelenvaldez07@gmail.com>
2020-10-21 19:42:51 -0300

    Added firebase and principal component

--- LaComanda/src/environments/environment.prod.ts
+++ LaComanda/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig : {
+    apiKey: "AIzaSyA4h5dtiB-MGyiIlf3vojyAlj4524giDx8",
+    authDomain: "lacomanda-a3726.firebaseapp.com",
+    databaseURL: "https://lacomanda-a3726.firebaseio.com",
+    projectId: "lacomanda-a3726",
+    storageBucket: "lacomanda-a3726.appspot.com",
+    messagingSenderId: "20914881890",
+    appId: "1:20914881890:web:0cc55db2b11aead8f8183b",
+    measurementId: "G-L16WNFL2MJ"
+  }
+
 };
--- LaComanda/src/environments/environment.ts
+++ LaComanda/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA4h5dtiB-MGyiIlf3vojyAlj4524giDx8",
+    authDomain: "lacomanda-a3726.firebaseapp.com",
+    databaseURL: "https://lacomanda-a3726.firebaseio.com",
+    projectId: "lacomanda-a3726",
+    storageBucket: "lacomanda-a3726.appspot.com",
+    messagingSenderId: "20914881890",
+    appId: "1:20914881890:web:0cc55db2b11aead8f8183b",
+    measurementId: "G-L16WNFL2MJ"
+  }
+
 };
 
 /*
